1a1837a512e52d7c1c69089026ff1d89fbdd17c4
yeltayev <nurdaulet.97@gmail.com>
2020-04-17 11:07:05 +0500

    Add monthly statistics

--- app/src/main/java/kz/yeltayev/aqms/module/place/PlaceViewModel.kt
+++ app/src/main/java/kz/yeltayev/aqms/module/place/PlaceViewModel.kt
@@ -19,10 +19,7 @@ import kz.yeltayev.aqms.api.WeatherApiServiceModule
 import kz.yeltayev.aqms.model.Gas
 import kz.yeltayev.aqms.model.Weather
 import kz.yeltayev.aqms.module.live.widget.PlaceUiModel
-import kz.yeltayev.aqms.module.place.widget.BarEntryData
-import kz.yeltayev.aqms.module.place.widget.DAYS_IN_WEEK
-import kz.yeltayev.aqms.module.place.widget.HOURS_IN_DAY
-import kz.yeltayev.aqms.module.place.widget.WeatherForecastUiModel
+import kz.yeltayev.aqms.module.place.widget.*
 import kz.yeltayev.aqms.utils.GeneralPreferences
 import kz.yeltayev.aqms.utils.ResourceProvider
 import kz.yeltayev.aqms.utils.round
@@ -61,11 +58,17 @@ class PlaceViewModel(
 
     private val disposable = CompositeDisposable()
 
-    /* WEEK STATISTICS */
-    val barData = ObservableField<BarData>()
-    val maxValue = ObservableInt()
-    val xAxis = ObservableField<List<String>>()
-    val yAxis = ObservableField<List<Float>>()
+    /* BAR CHART */
+    val weekBarData = ObservableField<BarData>()
+    val weekMaxValue = ObservableInt()
+    val weekXAxis = ObservableField<List<String>>()
+    val weekYAxis = ObservableField<List<Float>>()
+
+
+    val monthBarData = ObservableField<BarData>()
+    val monthMaxValue = ObservableInt()
+    val monthXAxis = ObservableField<List<String>>()
+    val monthYAxis = ObservableField<List<Float>>()
 
     private val serviceModule = ApiServiceModule()
 
@@ -81,7 +84,19 @@ class PlaceViewModel(
         res.getString(R.string.label_tgs2600),
         res.getString(R.string.label_tgs2602)
     )
-    /* WEEK STATISTICS */
+    /*  BAR CHART  */
+
+    fun setPlace(placeUiModel: PlaceUiModel) {
+        this.placeUiModel.set(placeUiModel)
+        tgs2600.set(placeUiModel.place.gas.tgs2600.round())
+        tgs2602.set(placeUiModel.place.gas.tgs2602.round())
+
+        fetchForecastData()
+        fetchWeatherData()
+        fetchGasData()
+
+        checkPrefs()
+    }
 
     private fun fetchForecastData() {
         isLoading.set(true)
@@ -120,16 +135,48 @@ class PlaceViewModel(
         )
     }
 
-    fun setPlace(placeUiModel: PlaceUiModel) {
-        this.placeUiModel.set(placeUiModel)
-        tgs2600.set(placeUiModel.place.gas.tgs2600.round())
-        tgs2602.set(placeUiModel.place.gas.tgs2602.round())
+    private fun fetchWeatherData() {
+        val placeService = serviceModule.getPlaceService()
+        val placeId = placeUiModel.get()?.place?.id ?: return
 
-        fetchForecastData()
-        fetchWeatherData()
-        fetchGasData()
+        disposable.add(
+            placeService.fetchWeatherById(placeId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSuccess { response ->
+                    val result = response.body() ?: return@doOnSuccess
+                    weathers.set(result)
 
-        checkPrefs()
+                    val data = getBarEntryDataByFilter()
+                    if (data != null) {
+                        drawWeekBarChart(data)
+                        drawMonthBarChart(data)
+                    }
+                }
+                .doOnError { error ->
+                    Timber.d("yeltayev22 $error")
+                }
+                .subscribe()
+        )
+    }
+
+    private fun fetchGasData() {
+        val placeService = serviceModule.getPlaceService()
+        val placeId = placeUiModel.get()?.place?.id ?: return
+
+        disposable.add(
+            placeService.fetchGasesById(placeId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSuccess { response ->
+                    val result = response.body() ?: return@doOnSuccess
+                    gases.set(result)
+                }
+                .doOnError { error ->
+                    Timber.d("yeltayev22 $error")
+                }
+                .subscribe()
+        )
     }
 
     private fun checkPrefs() {
@@ -173,7 +220,8 @@ class PlaceViewModel(
 
                 val data = getBarEntryDataByFilter()
                 if (data != null) {
-                    drawBarChart(data)
+                    drawWeekBarChart(data)
+                    drawMonthBarChart(data)
                 }
 
                 dialog.dismiss()
@@ -183,53 +231,9 @@ class PlaceViewModel(
             .show()
     }
 
-    private fun fetchWeatherData() {
-        val placeService = serviceModule.getPlaceService()
-        val placeId = placeUiModel.get()?.place?.id ?: return
-
-        disposable.add(
-            placeService.fetchWeatherById(placeId)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSuccess { response ->
-                    val result = response.body() ?: return@doOnSuccess
-                    weathers.set(result)
-
-                    val data = getBarEntryDataByFilter()
-                    if (data != null) {
-                        drawBarChart(data)
-                    }
-                }
-                .doOnError { error ->
-                    Timber.d("yeltayev22 $error")
-                }
-                .subscribe()
-        )
-    }
-
-    private fun fetchGasData() {
-        val placeService = serviceModule.getPlaceService()
-        val placeId = placeUiModel.get()?.place?.id ?: return
-
-        disposable.add(
-            placeService.fetchGasesById(placeId)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSuccess { response ->
-                    val result = response.body() ?: return@doOnSuccess
-                    gases.set(result)
-                }
-                .doOnError { error ->
-                    Timber.d("yeltayev22 $error")
-                }
-                .subscribe()
-        )
-    }
-
     @SuppressLint("SimpleDateFormat")
-    private fun drawBarChart(result: List<BarEntryData>) {
+    private fun drawWeekBarChart(result: List<BarEntryData>) {
         val barEntries = mutableListOf<BarEntry>()
-        result.sortedByDescending { it.dateTime }
 
         var index = 0
         val xAxis = mutableListOf<String>()
@@ -254,45 +258,103 @@ class PlaceViewModel(
             yAxis.add(value)
         }
 
-        this.xAxis.set(xAxis)
-        this.yAxis.set(yAxis)
+        xAxis.reverse()
+        this.weekXAxis.set(xAxis)
+        this.weekYAxis.set(yAxis)
 
         val maxCount = result.maxBy { it.value }?.value?.toInt()
         if (maxCount != null) {
-            maxValue.set(maxCount)
+            weekMaxValue.set(maxCount)
         }
 
+        barEntries.reverse()
         val barDataSet = BarDataSet(barEntries.sortedWith(compareBy { it.x }), "")
         barDataSet.setDrawValues(false)
-        barDataSet.highLightColor = res.getColor(R.color.darkBlue)
+        barDataSet.highLightColor = res.getColor(R.color.colorAccent)
         val data = BarData(barDataSet)
         data.barWidth = 0.19f
 
         if (barEntries.isNotEmpty()) {
-            barData.set(data)
+            weekBarData.set(data)
         } else {
-            barData.set(null)
+            weekBarData.set(null)
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun drawMonthBarChart(result: List<BarEntryData>) {
+        val barEntries = mutableListOf<BarEntry>()
+
+        var index = 0
+        val xAxis = mutableListOf<String>()
+        val yAxis = mutableListOf<Float>()
+        for (i in 0 until DAYS_IN_MONTH * HOURS_IN_DAY step HOURS_IN_DAY) {
+
+            val value = result[i].value
+            barEntries.add(
+                BarEntry(
+                    (index + 1).toFloat(),
+                    value
+                )
+            )
+            index = index.inc()
+
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+            val date = dateFormat.parse(result[i].dateTime)
+            val dayInMonthFormat = SimpleDateFormat("MMM d")
+            val dayInMonth = dayInMonthFormat.format(date)
+
+            xAxis.add(dayInMonth)
+            yAxis.add(value)
+        }
+
+        xAxis.reverse()
+        this.monthXAxis.set(xAxis)
+        this.monthYAxis.set(yAxis)
+
+        val maxCount = result.maxBy { it.value }?.value?.toInt()
+        if (maxCount != null) {
+            monthMaxValue.set(maxCount)
+        }
+
+        barEntries.reverse()
+        val barDataSet = BarDataSet(barEntries.sortedWith(compareBy { it.x }), "")
+        barDataSet.setDrawValues(false)
+        barDataSet.highLightColor = res.getColor(R.color.colorAccent)
+        val data = BarData(barDataSet)
+        data.barWidth = 0.19f
+
+        if (barEntries.isNotEmpty()) {
+            monthBarData.set(data)
+        } else {
+            monthBarData.set(null)
         }
     }
 
     private fun getBarEntryDataByFilter(): List<BarEntryData>? {
         val weathers = weathers.get() ?: return null
         val gases = gases.get() ?: return null
+
         return when (selectedFilter.get()) {
             res.getString(R.string.label_temperature) -> {
                 weathers.map { BarEntryData(it.temperature.toFloat(), it.dateTime) }
+                    .sortedByDescending { it.dateTime }
             }
             res.getString(R.string.label_pressure) -> {
                 weathers.map { BarEntryData(it.pressure.toFloat(), it.dateTime) }
+                    .sortedByDescending { it.dateTime }
             }
             res.getString(R.string.label_humidity) -> {
                 weathers.map { BarEntryData(it.humidity.toFloat(), it.dateTime) }
+                    .sortedByDescending { it.dateTime }
             }
             res.getString(R.string.label_tgs2600) -> {
                 gases.map { BarEntryData(it.tgs2600.toFloat(), it.dateTime) }
+                    .sortedByDescending { it.dateTime }
             }
             res.getString(R.string.label_tgs2602) -> {
                 gases.map { BarEntryData(it.tgs2602.toFloat(), it.dateTime) }
+                    .sortedByDescending { it.dateTime }
             }
             else -> null
         }
